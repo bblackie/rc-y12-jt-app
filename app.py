14c245cf8d505f1251ab9a4bb782fc41dd94d931
JaydenCT <221056@trinityschools.nz>
2025-03-26 13:00:37 +1300

    added colours/new code

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
-from flask import Flask 
+from flask import Flask, g, render_template 
+import sqlite3
+from flask import g
+
 
 app = Flask(__name__)
 
+DATABASE = 'database_cars.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
 @app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/cars")
 def index():
-    return "Hello, World!"
+    cursor = get_db().cursor()
+    sql = '''
+SELECT manufacturer, model, year, kms, price, type FROM cars
+JOIN Transmission
+ON Cars.transmission = Transmission.id
+ORDER BY manufacturer, model
+'''
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    return render_template ("cars.html" , cars=results)
 
 if __name__ == "__main__":
     app.run(debug=True)
